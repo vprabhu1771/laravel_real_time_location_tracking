7c77825eadb9ca974be63084eb761e46b312ab3a
Prabhu <vprabhu1771@gmail.com>
2026-03-03 16:46:45 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\LocationController;
+
+Route::get('/', [LocationController::class, 'index'])->name('location.index');
+
+Route::post('/location/update', [LocationController::class, 'update'])->name('location.update');
